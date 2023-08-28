85669039937962ff4ad7314632be66d8f054469d
nthnn <nathanneisip@gmail.com>
2023-08-29 02:02:57 +0800

    Initial Doxygen configuration.

--- examples/full_example/full_example.ino
+++ examples/full_example/full_example.ino
@@ -1,8 +1,10 @@
 #include <Adafruit_SSD1306.h>
 #include <PortaMob.h>
 
+void on_display_err();
+
 Adafruit_SSD1306 display =
-    PortaMob::portamob_display();
+    PortaMob::portamob_display(on_display_err);
 
 void setup() {
     Serial.begin(9600);
@@ -11,4 +13,9 @@ void setup() {
 
 void loop() {
 
+}
+
+void on_display_err() {
+    Serial.println("Cannot find OLED display.");
+    while(true);
 }
