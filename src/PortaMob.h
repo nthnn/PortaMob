09be20773997c7be473e8d670c71cc3b1ebffa33
nthnn <nathanneisip@gmail.com>
2023-08-29 02:05:24 +0800

    Code adjustments and workflow CI fixes.

--- src/PortaMob.h
+++ src/PortaMob.h
@@ -4,15 +4,15 @@
 #include <Arduino.h>
 #include <Adafruit_SSD1306.h>
 
-#define PORTAMOB_KEY_1 D2
-#define PORTAMOB_KEY_2 D3
-#define PORTAMOB_KEY_3 D4
-#define PORTAMOB_KEY_4 D5
-#define PORTAMOB_KEY_5 D6
-#define PORTAMOB_KEY_6 D7
-#define PORTAMOB_KEY_7 D8
-#define PORTAMOB_KEY_8 D9
-#define PORTAMOB_KEY_9 D10
+#define PORTAMOB_KEY_1 2
+#define PORTAMOB_KEY_2 3
+#define PORTAMOB_KEY_3 4
+#define PORTAMOB_KEY_4 5
+#define PORTAMOB_KEY_5 6
+#define PORTAMOB_KEY_6 7
+#define PORTAMOB_KEY_7 8
+#define PORTAMOB_KEY_8 9
+#define PORTAMOB_KEY_9 10
 
 #define PORTAMOB_EEPROM_ADDR 0x50
 
