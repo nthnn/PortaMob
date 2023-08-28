e4a9d80bfe203af9ada04ef872341960292beb8b
nthnn <nathanneisip@gmail.com>
2023-08-29 02:40:44 +0800

    Renamed pcb_file to pcb_files.

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -30,8 +30,7 @@ var NAVTREE =
       [ "Key Components", "index.html#autotoc_md1", null ],
       [ "Hardware Setup", "index.html#autotoc_md2", null ],
       [ "Software Setup", "index.html#autotoc_md3", null ],
-      [ "Schematics", "index.html#autotoc_md4", null ],
-      [ "License", "index.html#autotoc_md5", null ],
+      [ "License", "index.html#autotoc_md4", null ],
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ]
     ] ],
--- docs/navtreeindex0.js
+++ docs/navtreeindex0.js
@@ -2,9 +2,9 @@ var NAVTREEINDEX0 =
 {
 "PortaMob_8cpp_source.html":[1,0,0,0],
 "PortaMob_8h_source.html":[1,0,0,1],
-"annotated.html":[0,6],
-"classPortaMob.html":[0,6,0],
-"classes.html":[0,7],
+"annotated.html":[0,5],
+"classPortaMob.html":[0,5,0],
+"classes.html":[0,6],
 "dir_68267d1309a1af8e8297ef4c3efbcdba.html":[1,0,0],
 "files.html":[1,0],
 "index.html":[],
@@ -13,6 +13,5 @@ var NAVTREEINDEX0 =
 "index.html#autotoc_md2":[2],
 "index.html#autotoc_md3":[3],
 "index.html#autotoc_md4":[4],
-"index.html#autotoc_md5":[5],
 "pages.html":[]
 };
